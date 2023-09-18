91b89795951a18e157bafa87769105520ef1dbf1
jp12345q <verdad.194305@meycauayan.sti.edu.ph>
2023-09-18 18:20:41 +0800

    nawala yong styles

--- POS/POS.php
+++ POS/POS.php
@@ -2,7 +2,7 @@
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
-    <link rel="stylesheet" href="/style.css" />
+    <link rel="stylesheet" href="../styles.css" />
     <title>POS</title>
     <!--Style inside main Page -->
     <style>
