4e3d0c4756c2534f26f5863056aa1a4e0d25f92c
enachan1 <yourmotherknowsbest022@gmail.com>
2023-10-11 20:49:06 +0800

    for delete functionalities

---
+++ ProductMaintenance/delete_uom.php
@@ -0,0 +1,24 @@
+<?php
+
+include "../connectdb.php";
+
+if(isset($_GET['id'])) 
+$id = $_GET['id'];
+
+$sqlquery = "DELETE FROM `uom_db` WHERE `id` = $id";
+try {
+$sql_result = mysqli_query($sqlconn, $sqlquery);
+
+if(!$sql_result) {
+    die("Something's Wrong");
+}
+else {
+    header("Location: ProductMaintenance.php?msg=Deleted Successfully");
+    exit();
+}
+}
+
+catch (Exception $e) {
+    echo $e;
+}
+?>
