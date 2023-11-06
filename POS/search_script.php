cf3dffaf323d55cc2041028f4d81294fccb6b340
CaloyDeLeon <carlodeleon754@gmail.com>
2023-11-06 23:44:04 +0800

    live Displaying item in the table

--- POS/search_script.php
+++ POS/search_script.php
@@ -3,7 +3,7 @@ $con = mysqli_connect("localhost", "root", "", "juncathytest");
 
 if (isset($_GET['search'])) {
     $filtervalues = $_GET['search'];
-    $query = "SELECT * FROM items_db WHERE CONCAT(item_sku,item_name,item_category) LIKE '%$filtervalues%' ";
+    $query = "SELECT * FROM items_db WHERE CONCAT(item_sku,item_name,item_category,item_stocks) LIKE '%$filtervalues%'";
     $query_run = mysqli_query($con, $query);
 
     if ($query_run) {
@@ -12,6 +12,7 @@ if (isset($_GET['search'])) {
             echo '<td>' . $items['item_sku'] . '</td>';
             echo '<td>' . $items['item_name'] . '</td>';
             echo '<td>' . $items['item_category'] . '</td>';
+            echo '<td>' . $items['item_stocks'] . '</td>';
             echo '</tr>';
         }
     } 
