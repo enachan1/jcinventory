cd469239be5bc9622a143f841ded1c2af7a77916
CaloyDeLeon <carlodeleon754@gmail.com>
2023-11-10 23:18:02 +0800

    Fixing search item to search item

--- POS/search_script.php
+++ POS/search_script.php
@@ -3,21 +3,17 @@ $con = mysqli_connect("localhost", "root", "", "juncathytest");
 
 if (isset($_GET['search'])) {
     $filtervalues = $_GET['search'];
-    $query = "SELECT * FROM items_db WHERE CONCAT(item_sku,item_name,item_category,item_stocks) LIKE '%$filtervalues%'";
+    $query = "SELECT * FROM items_db WHERE CONCAT(item_sku,item_name,item_category,item_stocks,item_price) LIKE '%$filtervalues%'";
     $query_run = mysqli_query($con, $query);
 
-    if ($query_run) {
-        foreach ($query_run as $items) {
-            echo '<tr>';
-            echo '<td>' . $items['item_sku'] . '</td>';
-            echo '<td>' . $items['item_name'] . '</td>';
-            echo '<td>' . $items['item_category'] . '</td>';
-            echo '<td>' . $items['item_stocks'] . '</td>';
-            echo '</tr>';
-        }
-    } 
-    else {
-        echo '<tr><td colspan="3"></td></tr>';
+    foreach ($query_run as $items) {
+        echo '<tr class="search-result">';
+        echo '<td>' . $items['item_sku'] . '</td>';
+        echo '<td>' . $items['item_name'] . '</td>';
+        echo '<td>' . $items['item_category'] . '</td>';
+        echo '<td>' . $items['item_stocks'] . '</td>';
+        echo '<td>' . $items['item_price'] . '</td>';
+        echo '</tr>';
     }
 }
 ?>
