0829d04f05d4919d4c8eac58c64e18d7d2372891
jp12345q <verdad.194305@meycauayan.sti.edu.ph>
2023-11-12 15:33:11 +0800

    Script js for price inquery

---
+++ POS/priceinPOS.js
@@ -0,0 +1,47 @@
+
+            //priceinquiry
+            $(document).ready(function () {
+                // Handle input event on the priceinquiry form
+                $("#priceForm input[name='price']").on("input", function () {
+                    var searchQuery = $(this).val();
+
+                    // Make an AJAX request to fetch price results
+                    $.ajax({
+                        url: "price_inquiry.php",
+                        type: "GET",
+                        data: { price: searchQuery },
+                        dataType: "json",
+                        success: function (response) {
+                            var data = response.data;
+                            if (data.length > 0) {
+                                var price = data[0].item_price;
+                                $("#priceDisplay").text(price);
+
+                                // Remove the "Price" column from the table
+                                var modifiedData = data.map(function (item) {
+                                    return {
+                                        item_sku: item.item_sku,
+                                        item_name: item.item_name,
+                                        item_category: item.item_category,
+                                    };
+                                });
+
+                                // Update content in the table body
+                                var tableBody = $("#priceResults tbody");
+                                tableBody.empty(); // Clear existing content
+
+                                $.each(modifiedData, function (index, item) {
+                                    var row = $("<tr>");
+                                    row.append($("<td>").text(item.item_sku));
+                                    row.append($("<td>").text(item.item_name));
+                                    row.append($("<td>").text(item.item_category));
+                                    tableBody.append(row);
+                                });
+                            } else {
+                                $("#priceDisplay").text("Wala pa ang price nito");
+                                $("#priceResults tbody").empty(); // Clear table body
+                            }
+                        }
+                    });
+                });
+            });
