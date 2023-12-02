85837508368fb9e60e4efe9e30f3832d00584970
tenchan <yourmotherknowsbest022@gmail.com>
2023-12-02 13:13:46 +0800

    po insertion colum bad order changed to inventory_in

--- PurchaseOrder/addPurchaseOrder.php
+++ PurchaseOrder/addPurchaseOrder.php
@@ -11,7 +11,7 @@ $dateoftransaction = mysqli_real_escape_string($sqlconn, $_POST['dateTrans']);
 $expectedDelivery = mysqli_real_escape_string($sqlconn, $_POST['expectDel']);
 
 foreach ($_POST['PO_sku'] as $key => $value) {
-    $sql_query = "INSERT INTO `purchase_order_db`(`po_item_sku`,`po_item_name`, `po_qty`, `po_uom`, `po_category`, `po_item_price`, `po_dot`, `po_expdelivery`, `is_delivered`, `isBadOrder`, `vendor_id`) VALUES (? ,?, ?, ?, ?, ?, ?, ?, NULL, NULL, ?)";
+    $sql_query = "INSERT INTO `purchase_order_db`(`po_item_sku`,`po_item_name`, `po_qty`, `po_uom`, `po_category`, `po_item_price`, `po_dot`, `po_expdelivery`, `is_delivered`, `inventory_in`, `vendor_id`) VALUES (? ,?, ?, ?, ?, ?, ?, ?, NULL, 0, ?)";
     $stmt = $sqlconn->prepare($sql_query);
     
     $stmt->bind_param("ssssssssi", $value, $_POST['PO_itemname'][$key], $_POST['PO_qty'][$key], $_POST['PO_uom'][$key], $_POST['PO_category'][$key], $_POST['PO_price'][$key], $dateoftransaction, $expectedDelivery, $vendor_id);
--- PurchaseOrder/update-to-delivered.php
+++ PurchaseOrder/update-to-delivered.php
@@ -3,7 +3,7 @@ include "../connectdb.php";
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
     $vendor_id = mysqli_real_escape_string($sqlconn,$_GET['vdid']);
 
-    $sql_query = "UPDATE `purchase_order_db` SET `is_delivered`= 1,`isBadOrder`= 0
+    $sql_query = "UPDATE `purchase_order_db` SET `is_delivered`= 1, `inventory_in`= 0
             WHERE `vendor_id` = $vendor_id";
         $sql_result = mysqli_query($sqlconn, $sql_query);
 
